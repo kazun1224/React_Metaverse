4f939909d935228811d412f28a1161d459135f0a
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2023-01-21 23:52:30 +0900

    3Dモデルを表示して簡易的なメタバースを作成

---
+++ src/Rocket.js
@@ -0,0 +1,18 @@
+// import "aframe";
+// import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+// import rocketModel from"./models/rocket.glf"
+
+// function Rocket({x, y, z}) {
+//   const loader = new GLTFLoader();
+//   loader.load(rocketModel, (d) => {
+//     const entity = document.getElementById("rocket");
+//     entity.object3D.add(d.scene);
+//   });
+
+//   return (
+
+//       <a-entity id="rocket"  position={`${x} ${y} ${z}`} scale="15 15 15" rotation="0 0 40"></a-entity>
+//   );
+// }
+
+// export default Rocket;
